74509ef42c8e40799a62ec9ad3e88d1b2c1ff24c
Daniel Monteagudo <dmonte3210@gmail.com>
2017-05-15 21:55:15 -0400

    finished-ish the entire site

--- static/js/draw.js
+++ static/js/draw.js
@@ -2,12 +2,76 @@ var canvas, ctx;
 var pmouseX, pmouseY;
 var mouseDown;
 var strokeWeight = 10;
-var slider;
-var curColor;
+var timer, counter;
+var curColor, drawPreview;
+var drawbtn, erasebtn, slider;
+var savedImage;
+var imgString;
+
+var stoImage = function() {
+    alert("DOING SHIT")
+    imgString = canvas.toDataURL("image/jpeg");
+    document.body.removeChild(canvas);
+    document.body.removeChild(drawPreview);
+    document.body.removeChild(document.getElementById("drawtools"));
+    savedImage.src = imgString;
+    document.getElementById("upload").value = imgString;
+    var submitbtn = document.createElement("input");
+    submitbtn.type = "submit";
+    submitbtn.value = "Upload Picture";
+    document.getElementById("datForm").appendChild(submitbtn);
+};
+
+var countDown = function(e) {
+    timer.innerHTML = parseInt(timer.innerHTML) - 1;
+    console.log(timer.innerHTML);
+    if (timer.innerHTML == "0") {
+	window.clearInterval(counter);
+    }
+}
+
+var draw = function(e) {
+    drawPreview.style.left = e.clientX - slider.value/2;
+    drawPreview.style.top = e.clientY - slider.value/2;
+    
+    if (pmouseX > 0 && pmouseY > 0 && mouseDown) {
+	if (drawbtn.checked) {
+	    ctx.fillStyle = curColor;
+	    ctx.strokeStyle = curColor;
+	    drawPreview.style.borderColor = curColor;
+	} else {
+	    if (canvas.style.backgroundColor == "") {
+		ctx.fillStyle = "#FFFFFF";
+		ctx.strokeStyle = "#FFFFFF";
+		drawPreview.style.borderColor = "#FFFFFF";
+	    } else {
+		ctx.fillStyle = canvas.style.backgroundColor;
+		ctx.strokeStyle = canvas.style.backgroundColor;
+		drawPreview.style.borderColor = canvas.style.backgroundColor;
+	    }
+	}
+	ctx.beginPath();
+	ctx.arc(pmouseX, pmouseY, strokeWeight/2, 0, 2 * Math.PI, false);
+	ctx.fill();
+	ctx.beginPath();
+	ctx.moveTo(pmouseX, pmouseY);
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.lineWidth = strokeWeight;
+	ctx.stroke();
+    }
+    pmouseX = e.offsetX;
+    pmouseY = e.offsetY;
+};
+
+window.setTimeout(stoImage, 50000);
+counter = window.setInterval(countDown, 1000);
 
 window.onload = function() {
     canvas = document.getElementById("doodle");
     ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FFFFFF";
+    ctx.rect(0,0,canvas.width, canvas.height);
+    ctx.fill();
     pmouseX = -1;
     pmouseY = -1;
     curColor = "#000000"
@@ -16,6 +80,8 @@ window.onload = function() {
     drawbtn = document.getElementById("draw");
     erasebtn = document.getElementById("erase");
     drawPreview = document.getElementById("drawcircle");
+    timer = document.getElementById("timeleft");
+    savedImage = document.getElementById("savedImg");
     clear.onclick = function(e) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
     };
@@ -30,41 +96,13 @@ window.onload = function() {
 	drawPreview.style.height = slider.value;
 	drawPreview.style.width = slider.value;
 	drawPreview.style.borderWidth = slider.value/20;
+	document.getElementById("strokeweight").innerHTML = "Stroke Width: " + slider.value;
     }
-    canvas.onmousemove = function(e) {
-	drawPreview.style.left = e.clientX - slider.value/2;
-	drawPreview.style.top = e.clientY - slider.value/2;
-	
-	if (pmouseX > 0 && pmouseY > 0 && mouseDown) {
-	    if (drawbtn.checked) {
-		console.log(drawbtn.checked);
-		ctx.fillStyle = curColor;
-		ctx.strokeStyle = curColor;
-		drawPreview.style.borderColor = curColor;
-	    } else {
-		if (canvas.style.backgroundColor == "") {
-		    ctx.fillStyle = "#FFFFFF";
-		    ctx.strokeStyle = "#FFFFFF";
-		    drawPreview.style.borderColor = "#FFFFFF";
-		} else {
-		    ctx.fillStyle = canvas.style.backgroundColor;
-		    ctx.strokeStyle = canvas.style.backgroundColor;
-		    drawPreview.style.borderColor = canvas.style.backgroundColor;
-		}
-	    }
-	    ctx.beginPath();
-	    ctx.arc(pmouseX, pmouseY, strokeWeight/2, 0, 2 * Math.PI, false);
-	    ctx.fill();
-	    ctx.beginPath();
-	    ctx.moveTo(pmouseX, pmouseY);
-	    ctx.lineTo(e.offsetX, e.offsetY);
-	    ctx.lineWidth = strokeWeight;
-	    ctx.stroke();
-	}
-	    pmouseX = e.offsetX;
-	    pmouseY = e.offsetY;
+    canvas.onmousemove = function(e) {draw(e);}
+    canvas.onmousedown = function(e) {
+	mouseDown = true;
+	draw(e);
     };
-    canvas.onmousedown = function(e) { mouseDown = true; };
     canvas.onmouseup = function(e) {
 	mouseDown = false;
 	pmouseX = -1;
