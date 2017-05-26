d99734524858861ea0113a51c7efb92ba67efc62
Daniel Monteagudo <dmonte3210@gmail.com>
2017-05-26 14:33:25 -0400

    noot noot

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import requests as req
 from utils import database as db
 import random
 
-#ADD COMMENTS DANIEL
+# CHILL WITH THE ALL CAPS EDMOND LAM
 
+# Declaring app and secret key for session encryption
 app = Flask(__name__)
 app.secret_key = 'Maddy says hi'
 
@@ -15,6 +16,8 @@ def get_new_word():
     wordl.close()
     return noun
 
+# Homepage
+# Either redirects to the login page or shows you index.html
 @app.route('/')
 def mainpage():
     if 'username' in session:
@@ -22,38 +25,53 @@ def mainpage():
     else:
         return redirect(url_for('login'))
 
+# Draw page
+# The user clicks on the button on the homepage and is sent to this page to submit their drawing
 @app.route('/draw')
 def draw():
+    # If there are matches in the database, select the word and get match info from there
     if db.matches_available():
         match_info = db.get_existing_match()
         word = match_info['word']
         match_id = match_info['match_id']
+    # Otherwise, pick a random word and make a match with that word with current user as user_1
     else:
         word = get_new_word()
         match_id = db.make_new_match(word, session['username'])
-        
+    # Store the match_id for upload
     session['match_id'] = match_id
     return render_template('match.html', word=word)
 
+# Judging page
+# If you get the page you're given two photos from a match without a winner
+# If you post to the route, it adds a winner to the match and returns you to the index page
 @app.route('/judge', methods=['GET', 'POST'])
 def judge():
+    # Gets a judgable match and passes it to judge.html
     if request.method == 'GET':
         matchdata = db.get_judgable_match()
         session['matchdata'] = matchdata
         return render_template('judge.html', matchdata=matchdata)
+    # Removes matchdata from the session so it's not cluttered then sets the winner in the database
     else:
         matchid = session.pop('matchdata')['match_id']
         winuser = str(int(request.form['winner']) + 1)
         db.pick_winner(matchdata['match_id'], winuser)
         matchdata = db.get_match(matchid)
-        return render_template('gallery.html', matchdata = matchdata)#'<h1>User %s won match %s with:</h1> <img src="%s"/>' % (matchdata['user_' + winuser], matchdata['match_id'], matchdata['img_' + winuser])
+        return render_template('gallery.html', matchdata = matchdata)
 
+# Profile page
+# Displays all of the matches the user submitted to with the winner highlighted somehow
 @app.route('/profile/<username>')
 def profile(username):
     matches = db.get_matches_for_user(username)
-    print matches
+    # print matches
+    # Edmond's parents had a little Lam
+    # Who's groups devlog was empty like the Sahara desert
     return render_template('profile.html', matches = matches)
-                           
+
+# Upload route
+# Uploads the photo to cloudinary and then updates the database with the picurl
 @app.route('/uploadPic', methods=['POST'])
 def upload():
     things = { 'file' : request.form['pic'], 'upload_preset' : 'bf17cjwp' }
@@ -61,16 +79,22 @@ def upload():
     picurl = upload.json()['secure_url']
     if 'match_id' in session:
         match_id = session.pop('match_id')
+    # This would be awkward
     else:
         return 'yikes'
+    # If the game exists, update it with the picurl.
     if db.game_exists(match_id):
         db.update_match(match_id, session['username'], picurl)
     return picurl + "<br>" + str(match_id)
 
+# Login route
+# Either shows the login page or validates things
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # If the page is begotten, show the page
     if request.method == 'GET':
         return render_template('login.html')
+    # Otherwise, *shrug*
     else:
         if 'username' in session:
             return redirect(url_for('mainpage'))
@@ -82,21 +106,24 @@ def login():
 
         if 'register' in request.form:
             db.create_user(username,str(password))
+            # Does not update the devlog
             return render_template('login.html', msg="Successfully registered!", good=True)
-    
+        
         elif db.check_login(username, str(password)):
             session['username'] = username;
             return redirect(url_for('mainpage'))
         else:
             return render_template('login.html', msg="Login invalid.", good=False)
 
-@app.route("/logout/")
+# Xinhui did this. I am not koalafied to comment on this code
+@app.route("/logout")
 def logout():
     if 'username' not in session:
         redirect(url_for('login'))
     session.pop('username')
     return redirect(url_for("login", msg = "Logged out.", good=True))
 
+# Who actually knows what this does. Some sort of black magic I think
 if __name__ == '__main__':
     app.debug = True
     app.run()
